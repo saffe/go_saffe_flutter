4a3887f34ea59d6c2eae8ff954282fad113c7d4c
Pedro Cruz <pedrovictor.lage@gmail.com>
2024-03-12 13:21:44 -0300

    Remove erro while check device
    If checking device returns error, the webview is render anyways without device context

--- lib/go_saffe_flutter.dart
+++ lib/go_saffe_flutter.dart
@@ -7,7 +7,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:safe_device/safe_device.dart';
 
-/// Go Saffe Capture Widget.
 class GoSaffeCapture extends StatefulWidget {
   final String apiKey;
   final String user;
@@ -46,7 +45,7 @@ class _CaptureState extends State<GoSaffeCapture> {
   String url = "";
   bool isLoading = true;
   final urlController = TextEditingController();
-  late Future<Map<String, bool>> _safeDeviceInfoFuture;
+  late Future<Map<String, bool>?> _safeDeviceInfoFuture;
 
   @override
   void initState() {
@@ -56,7 +55,7 @@ class _CaptureState extends State<GoSaffeCapture> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Map<String, bool>>(
+    return FutureBuilder<Map<String, bool>?>(
         future: _safeDeviceInfoFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -66,121 +65,110 @@ class _CaptureState extends State<GoSaffeCapture> {
               ),
             );
           } else {
-            if (snapshot.hasError) {
-              return Scaffold(
-                body: Center(
-                  child: Text('Error while checking the device.'),
-                ),
-              );
-            } else {
-              return Scaffold(
-                  body: SafeArea(
-                      child: Column(children: <Widget>[
-                Expanded(
-                  child: Stack(
-                    children: [
-                      InAppWebView(
-                        key: webViewKey,
-                        initialUrlRequest: URLRequest(
-                          url: WebUri("https://go.saffe.ai/v0/capture"),
-                          method: "POST",
-                          body: Uint8List.fromList(utf8.encode(jsonEncode({
-                            "api_key": widget.apiKey,
-                            "user_identifier": widget.user,
-                            "type": widget.type,
-                            "end_to_end_id": widget.endToEndId,
-                            "device": snapshot.data,
-                          }))),
-                          headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                          },
-                        ),
-                        initialSettings: settings,
-                        onWebViewCreated: (controller) {
-                          webViewController = controller;
-                        },
-                        onLoadStart: (controller, url) {
-                          setState(() {
-                            this.url = url.toString();
-                            urlController.text = this.url;
-                          });
-                        },
-                        onPermissionRequest: (controller, request) async {
-                          return PermissionResponse(
-                              resources: request.resources,
-                              action: PermissionResponseAction.GRANT);
-                        },
-                        shouldOverrideUrlLoading:
-                            (controller, navigationAction) async {
-                          return NavigationActionPolicy.ALLOW;
-                        },
-                        onLoadStop: (controller, url) async {
-                          setState(() {
-                            this.url = url.toString();
-                            urlController.text = this.url;
-                          });
-
-                          controller.addJavaScriptHandler(
-                              handlerName: 'receiveMessage',
-                              callback: (args) {
-                                final source = args[0]['source'];
-                                final event = args[0]['payload']['event'];
-
-                                if (source == 'go-saffe-capture' &&
-                                    event == 'finish') {
-                                  if (widget.onFinish != null) {
-                                    widget.onFinish!();
-                                  }
-                                }
-
-                                if (source == 'go-saffe-capture' &&
-                                    event == 'close') {
-                                  if (widget.onClose != null) {
-                                    widget.onClose!();
-                                  }
-                                }
-                              });
-
-                          controller.evaluateJavascript(source: '''
-                            window.addEventListener('message', function(event) {
-                              // Verifica se a mensagem é do tipo esperado
-                              if (event.data && event.data.source === 'go-saffe-capture' && event.data.payload.event === 'finish') {
-                                // Envia a mensagem para o callback do Flutter
-                                window.flutter_inappwebview.callHandler('receiveMessage', event.data);
-                              }
-                            });
-                          ''');
-                        },
-                        onProgressChanged: (controller, progress) {
-                          if (progress >= 100) {
-                            setState(() {
-                              isLoading = false;
-                            });
-                          }
-                        },
-                        onUpdateVisitedHistory:
-                            (controller, url, androidIsReload) {
-                          setState(() {
-                            this.url = url.toString();
-                            urlController.text = this.url;
-                          });
-                        },
-                        onConsoleMessage: (controller, consoleMessage) {},
-                        onReceivedError: (controller, request, error) {
-                          if (widget.onError != null) {
-                            widget.onError!();
-                          }
+            return Scaffold(
+                body: SafeArea(
+                    child: Column(children: <Widget>[
+              Expanded(
+                child: Stack(
+                  children: [
+                    InAppWebView(
+                      key: webViewKey,
+                      initialUrlRequest: URLRequest(
+                        url: WebUri("https://go.saffe.ai/v0/capture"),
+                        method: "POST",
+                        body: Uint8List.fromList(utf8.encode(jsonEncode({
+                          "api_key": widget.apiKey,
+                          "user_identifier": widget.user,
+                          "type": widget.type,
+                          "end_to_end_id": widget.endToEndId,
+                          "device_context": snapshot.data,
+                        }))),
+                        headers: {
+                          'Content-Type': 'application/x-www-form-urlencoded',
                         },
                       ),
-                      if (isLoading)
-                        const Center(
-                            child:
-                                CircularProgressIndicator(color: Colors.cyan)),
-                    ],
-                  ),
+                      initialSettings: settings,
+                      onWebViewCreated: (controller) {
+                        webViewController = controller;
+                      },
+                      onLoadStart: (controller, url) {
+                        setState(() {
+                          this.url = url.toString();
+                          urlController.text = this.url;
+                        });
+                      },
+                      onPermissionRequest: (controller, request) async {
+                        return PermissionResponse(
+                            resources: request.resources,
+                            action: PermissionResponseAction.GRANT);
+                      },
+                      shouldOverrideUrlLoading:
+                          (controller, navigationAction) async {
+                        return NavigationActionPolicy.ALLOW;
+                      },
+                      onLoadStop: (controller, url) async {
+                        setState(() {
+                          this.url = url.toString();
+                          urlController.text = this.url;
+                        });
+
+                        controller.addJavaScriptHandler(
+                            handlerName: 'receiveMessage',
+                            callback: (args) {
+                              final source = args[0]['source'];
+                              final event = args[0]['payload']['event'];
+
+                              if (source == 'go-saffe-capture' &&
+                                  event == 'finish') {
+                                if (widget.onFinish != null) {
+                                  widget.onFinish!();
+                                }
+                              }
+
+                              if (source == 'go-saffe-capture' &&
+                                  event == 'close') {
+                                if (widget.onClose != null) {
+                                  widget.onClose!();
+                                }
+                              }
+                            });
+
+                        controller.evaluateJavascript(source: '''
+                          window.addEventListener('message', function(event) {
+                            if (event.data && event.data.source === 'go-saffe-capture' && event.data.payload.event === 'finish') {
+                              window.flutter_inappwebview.callHandler('receiveMessage', event.data);
+                            }
+                          });
+                        ''');
+                      },
+                      onProgressChanged: (controller, progress) {
+                        if (progress >= 100) {
+                          setState(() {
+                            isLoading = false;
+                          });
+                        }
+                      },
+                      onUpdateVisitedHistory:
+                          (controller, url, androidIsReload) {
+                        setState(() {
+                          this.url = url.toString();
+                          urlController.text = this.url;
+                        });
+                      },
+                      onConsoleMessage: (controller, consoleMessage) {},
+                      onReceivedError: (controller, request, error) {
+                        if (widget.onError != null) {
+                          widget.onError!();
+                        }
+                      },
+                    ),
+                    if (isLoading)
+                      const Center(
+                          child: CircularProgressIndicator(color: Colors.cyan)),
+                  ],
                 ),
-              ])));
-            }
+              ),
+            ])));
           }
         });
   }
